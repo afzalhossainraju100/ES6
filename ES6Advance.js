aad604d07d9398a00741d885d97d40eb84ce3f5d
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-10-08 03:08:25 +0600

    Learn about reduce and find

--- ES6Advance.js
+++ ES6Advance.js
@@ -13,3 +13,23 @@ console.log(student[1]);
 console.log(student["name"]);
 console.log(student["age"]);
 console.log(student["1"]);
+//map js
+const number = [1,2,3,4,5];
+const square = number.map((x)=> x*2);
+console.log(square);
+//fileter js
+const even = number.filter((x)=>x%2===0);
+console.log(even);
+square[1]=20;
+console.log(number);
+console.log(square);
+//for each
+number.forEach((x)=>console.log(x*3));
+
+console.log(number);
+//find
+firstEven = number.find((x)=>x%2===0);
+console.log(firstEven);
+//reduce
+const sum = square.reduce((acc,curr)=> acc+curr,0);
+console.log(sum);
