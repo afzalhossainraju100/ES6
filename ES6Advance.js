eef36c1327a053e427a9f72ab645cf7041fc8ab7
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-10-07 22:47:00 +0600

    dot notation

---
+++ ES6Advance.js
@@ -0,0 +1,15 @@
+//dot notation
+const student ={
+    name: "John",
+    age: 20,
+    1:50,
+}
+
+console.log(student.name);
+console.log(student.age);
+console.log(student[1]);
+
+//bracket notation
+console.log(student["name"]);
+console.log(student["age"]);
+console.log(student["1"]);
