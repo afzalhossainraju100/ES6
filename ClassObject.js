916eed5e7557b0a66de6153623b8ca8e91692bf3
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-10-08 03:18:19 +0600

    basic object and class creatioin and move to OOP of JS ES6

---
+++ ClassObject.js
@@ -0,0 +1,14 @@
+class Player{
+    constructor(){
+        console.log('Player is created');
+    }
+    goal(){
+        console.log('GOAL!!!');
+    }
+
+}
+    const player1 = new Player();
+    player1.goal();
+    console.log(player1);
+
+    // basic class and object
